60c739f8b3fcc0d62d54bcbb68b1a6a3fbab13e9
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-18 10:00:47 +0700

    Ola

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-import { Button } from "@mantine/core";
+import React from "react";
+import { Slider, Button } from "@mantine/core";
+
+function Demo() {
+  const sliderRef = React.useRef<HTMLInputElement>(null);
+  return (
+    <>
+      <Slider
+        ref={sliderRef}
+        marks={[
+          { value: 20, label: "20%" },
+          { value: 50, label: "50%" },
+          { value: 80, label: "80%" },
+        ]}
+      />
+
+      <Button
+        onClick={() => {
+          console.log(sliderRef.current);
+        }}
+      >
+        Print value
+      </Button>
+    </>
+  );
+}
 
 export default function App() {
-  return <Button>Hello world!</Button>;
+  return <Demo />;
 }
